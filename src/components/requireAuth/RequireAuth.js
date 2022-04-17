d05fed0f5ab9366d9028ec146d39bfde3996ee12
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-04-17 20:58:23 +0600

    email verification updated

--- src/components/requireAuth/RequireAuth.js
+++ src/components/requireAuth/RequireAuth.js
@@ -1,14 +1,55 @@
 import React from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
+import {
+  useAuthState,
+  useSendEmailVerification,
+} from "react-firebase-hooks/auth";
 import { Navigate, useLocation } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 import auth from "../../Firebase/firebase.init";
+import Loading from "../shared/Loading/Loading";
 
 const RequireAuth = ({ children }) => {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const location = useLocation();
-  if (!user) {
-    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+  const [sendEmailVerification, sending, error] =
+    useSendEmailVerification(auth);
+  if (loading) {
+    return <Loading></Loading>;
   }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  if (!user.emailVerified) {
+    return (
+      <div className="text-center flex justify-center items-center h-[70vh]">
+        <div>
+          <h3 className="text-2xl font-semibold">
+            Your Email is not verified!!
+          </h3>
+          <h5 className="text-2xl mb-5 font-semibold">
+            Please Verify your email address
+          </h5>
+          <span className="text-xl font-semibold text-gray-600">
+            Clik here{" "}
+            <button
+              className="text-blue-500"
+              onClick={async () => {
+                await sendEmailVerification();
+                toast("Sent email");
+              }}
+            >
+              Send a verification email again
+            </button>
+          </span>
+        </div>
+        <ToastContainer />
+      </div>
+    );
+  }
+
   return children;
 };
+
 export default RequireAuth;
